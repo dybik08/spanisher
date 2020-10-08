54490ee091359f813be6bb3c1ab5543c313f6a10
Wojciech Dybikowski <wojciech.dybikowski@profil-software.com>
2020-10-08 23:24:07 +0200

    initial & basic exercise template & prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SelectWordsLesson from "./Components/Lessons/SelectWordsLesson";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Spanish/English language learning app
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div>
+        <SelectWordsLesson/>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Lessons/SelectWordsLesson.tsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+
+const words = ['es', 'mi', 'madre', 'Esta'];
+const correctSentence = 'Esta es mi madre';
+
+const SelectWordsLesson = () => {
+    const [answers, setAnwsers] = useState<string[]>(Array.from({ length: words.length }));
+
+    const renderAnswers = Array.from({ length: 4 }).map((el, index) => (
+        <p
+            style={{
+                width: '50px',
+                borderBottom: !answers[index] ? '1px solid black' : undefined,
+                margin: '30px 40px',
+            }}
+        >
+            {answers[index] || ''}
+        </p>
+    ));
+
+    const onAnswerButtonPress = (value: string, index: number) => {
+        setAnwsers((prevState: string[]) => {
+            const newAnswers = [...prevState];
+            if (!answers[0]) {
+                newAnswers[0] = value;
+            } else if (!answers[1]) {
+                newAnswers[1] = value;
+            } else if (!answers[2]) {
+                newAnswers[2] = value;
+            } else if (!answers[3]) {
+                newAnswers[3] = value;
+            }
+
+            return newAnswers;
+        });
+    };
+
+    const renderAnswerButtons = words.map((word, index) => (
+        <button
+            onClick={(event: React.MouseEvent<HTMLButtonElement> & { target: HTMLInputElement }) =>
+                onAnswerButtonPress(word, index)
+            }
+            value={'es'}
+            style={{ margin: '0px 10px' }}
+        >
+            {word}
+        </button>
+    ));
+
+    const checkExercise = () => {
+        const isCorrect = answers.join(' ') === correctSentence;
+
+        return setTimeout(() => alert(isCorrect ? 'Gratz!' : 'Try again!'));
+    };
+
+    return (
+        <>
+            <h2>Translate sentence</h2>
+            <h2>This is my mother</h2>
+            <div style={{ display: 'flex', flexDirection: 'row', textAlign: 'center', justifyContent: 'center' }}>
+                {renderAnswers}
+            </div>
+            <div>{renderAnswerButtons}</div>
+            {answers.every(answer => answer) && (
+                <div style={{ marginTop: '30px' }}>
+                    <button onClick={checkExercise}>Check execrice</button>
+                </div>
+            )}
+        </>
+    );
+};
+
+export default SelectWordsLesson;
